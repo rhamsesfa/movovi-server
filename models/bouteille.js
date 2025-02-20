683ac3c48823051a5cb20687443875685259858e
Glitch (quill-observant-bell) <none>
2025-02-20 17:07:28 +0000

    🙋👀 Checkpoint ./models/bouteille.js:56515816/28

--- models/bouteille.js
+++ models/bouteille.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const bouteilleSchema = new mongoose.Schema({
     type: { type: String, enum: ['pleine', 'vide'], required: true },
     estConsigne: { type: Boolean, default: false },
-    livraison: { type: mongoose.Schema.Types.ObjectId, ref: 'Livraison' }
+    livraison: { type: mongoose.Schema.Types.ObjectId, ref: 'Livraison' },
+    date: { type: Date }
 });
 
 module.exports = mongoose.model('Bouteille', bouteilleSchema);
