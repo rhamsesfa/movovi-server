93e2505362ad822140425876698b36402e31d846
Glitch (quill-observant-bell) <none>
2025-02-20 16:55:27 +0000

    ☕️🐈 Checkpoint ./models/camion.js:56515816/28

--- models/camion.js
+++ models/camion.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const camionSchema = new mongoose.Schema({
     immatriculation: { type: String, required: true },
     capacite: { type: Number, required: true },
-    livraisons: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Livraison' }]
+    livraisons: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Livraison' }],
+    date: { type: Date }
 });
 
 module.exports = mongoose.model('Camion', camionSchema);
