d4eee466f50d40feb188c4f7f2a4d166647b6436
Glitch (quill-observant-bell) <none>
2025-04-07 18:40:27 +0000

    👽🌗 Checkpoint ./middleware/formatFrenchInput.js:56515816/1584

--- middleware/formatFrenchInput.js
+++ middleware/formatFrenchInput.js
@@ -8,10 +8,11 @@ module.exports = (req, res, next) => {
 
     // 2. Formatage intelligent
     req.body.french = req.body.french
-      // Supprime les apostrophes en fin de mot
-      .replace(/(\w)['’]+(\s|$)/g, '$1$2')  // Fin de mot
-      // Supprime les apostrophes en début de mot
-      .replace(/(^|\s)['’]+(\w)/g, '$1$2')  // Début de mot
+      // Supprime tous les caractères non autorisés (sauf apostrophes au milieu des mots)
+      .replace(/(\w)['’](\w)|['’]|([^a-zA-ZÀ-ÿ0-9\s'])/g, (match, p1, p2) => {
+        // Garde uniquement les apostrophes entre lettres (p1 et p2 capturent les lettres autour)
+        return p1 && p2 ? `${p1}'${p2}` : '';
+      })
       // Normalisation des espaces
       .replace(/\s+/g, ' ')
       .trim()
